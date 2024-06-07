ab7a750330feb9b0635abc60fc52e403c805654e
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-06-07 22:52:21 +0000

    tasks, appointments, tags and notes added

--- src/Client.php
+++ src/Client.php
@@ -7,11 +7,19 @@ namespace MusheAbdulHakim\GoHighLevel;
 use MusheAbdulHakim\GoHighLevel\Contracts\ClientContract;
 use MusheAbdulHakim\GoHighLevel\Contracts\Resources\BusinessContract;
 use MusheAbdulHakim\GoHighLevel\Contracts\Resources\CompanyContract;
+use MusheAbdulHakim\GoHighLevel\Contracts\Resources\Contacts\AppointmentContract;
 use MusheAbdulHakim\GoHighLevel\Contracts\Resources\Contacts\ContactContract;
+use MusheAbdulHakim\GoHighLevel\Contracts\Resources\Contacts\NoteContract;
+use MusheAbdulHakim\GoHighLevel\Contracts\Resources\Contacts\TagContract;
+use MusheAbdulHakim\GoHighLevel\Contracts\Resources\Contacts\Task;
+use MusheAbdulHakim\GoHighLevel\Contracts\Resources\Contacts\TaskContract;
 use MusheAbdulHakim\GoHighLevel\Contracts\TransporterContract;
 use MusheAbdulHakim\GoHighLevel\Resources\Business;
 use MusheAbdulHakim\GoHighLevel\Resources\Company;
+use MusheAbdulHakim\GoHighLevel\Resources\Contacts\Appointment;
 use MusheAbdulHakim\GoHighLevel\Resources\Contacts\Contact;
+use MusheAbdulHakim\GoHighLevel\Resources\Contacts\Note;
+use MusheAbdulHakim\GoHighLevel\Resources\Contacts\Tag;
 
 final class Client implements ClientContract
 {
@@ -36,5 +44,25 @@ final class Client implements ClientContract
     public function contacts(): ContactContract
     {
         return new Contact($this->transporter);
+    }
+
+    public function tasks(): TaskContract
+    {
+        return new Task($this->transporter);
+    }
+
+    public function appointments(): AppointmentContract
+    {
+        return new Appointment($this->transporter);
+    }
+
+    public function tags(): TagContract
+    {
+        return new Tag($this->transporter);
+    }
+
+    public function notes(): NoteContract
+    {
+        return new Note($this->transporter);
     }
 }
