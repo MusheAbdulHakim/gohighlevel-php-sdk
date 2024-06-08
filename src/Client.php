dc5e915917af469c96c6546a47ec047531f7abbf
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-06-08 16:10:31 +0000

    calendars, campaigns, companies and course resources added

--- src/Client.php
+++ src/Client.php
@@ -6,14 +6,20 @@ namespace MusheAbdulHakim\GoHighLevel;
 
 use MusheAbdulHakim\GoHighLevel\Contracts\ClientContract;
 use MusheAbdulHakim\GoHighLevel\Contracts\Resources\BusinessContract;
+use MusheAbdulHakim\GoHighLevel\Contracts\Resources\Calendars\CalendarContract;
+use MusheAbdulHakim\GoHighLevel\Contracts\Resources\Campaigns\CampaignContract;
 use MusheAbdulHakim\GoHighLevel\Contracts\Resources\CompanyContract;
 use MusheAbdulHakim\GoHighLevel\Contracts\Resources\Contacts\ContactContract;
 use MusheAbdulHakim\GoHighLevel\Contracts\Resources\Conversations\ConversationContract;
+use MusheAbdulHakim\GoHighLevel\Contracts\Resources\Courses\CourseContract;
 use MusheAbdulHakim\GoHighLevel\Contracts\TransporterContract;
 use MusheAbdulHakim\GoHighLevel\Resources\Business;
+use MusheAbdulHakim\GoHighLevel\Resources\Calendars\Calendar;
+use MusheAbdulHakim\GoHighLevel\Resources\Campaigns\Campaign;
 use MusheAbdulHakim\GoHighLevel\Resources\Company;
 use MusheAbdulHakim\GoHighLevel\Resources\Contacts\Contact;
 use MusheAbdulHakim\GoHighLevel\Resources\Conversations\Conversation;
+use MusheAbdulHakim\GoHighLevel\Resources\Courses\Course;
 
 final class Client implements ClientContract
 {
@@ -30,6 +36,16 @@ final class Client implements ClientContract
         return new Business($this->transporter);
     }
 
+    public function calendars(): CalendarContract
+    {
+        return new Calendar($this->transporter);
+    }
+
+    public function campaigns(): CampaignContract
+    {
+        return new Campaign($this->transporter);
+    }
+
     public function companies(): CompanyContract
     {
         return new Company($this->transporter);
@@ -43,5 +59,10 @@ final class Client implements ClientContract
     public function conversations(): ConversationContract
     {
         return new Conversation($this->transporter);
+    }
+
+    public function courses(): CourseContract
+    {
+        return new Course($this->transporter);
     }
 }
