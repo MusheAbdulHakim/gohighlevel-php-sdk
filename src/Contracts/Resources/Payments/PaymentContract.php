abf49cca4b23f55942327f7b9d9819f43c619c01
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-06-15 19:27:42 +0000

    payments api added

--- src/Contracts/Resources/Payments/PaymentContract.php
+++ src/Contracts/Resources/Payments/PaymentContract.php
@@ -4,4 +4,15 @@ namespace MusheAbdulHakim\GoHighLevel\Contracts\Resources\Payments;
 
 interface PaymentContract
 {
+    public function integration(): IntegrationContract;
+
+    public function order(): OrderContract;
+
+    public function orderFulfillment(): OrderFulfillmentContract;
+
+    public function transaction(): TransactionContract;
+
+    public function subscription(): SubscriptionContract;
+
+    public function customProvider(): CustomProviderContract;
 }
