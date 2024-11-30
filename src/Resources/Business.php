a177a35389d68ae74fa9726514622cdc634149c1
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-11-30 01:57:36 +0000

    typing and method docs updated

--- src/Resources/Business.php
+++ src/Resources/Business.php
@@ -11,42 +11,42 @@ final class Business implements BusinessContract
 {
     use Concerns\Transportable;
 
-    public function update(string $businessId, array $params): \MusheAbdulHakim\GoHighLevel\ValueObjects\Transporter\Response
+    public function update(string $businessId, array $params = []): array|string
     {
         $payload = Payload::put("businesses/$businessId", $params);
 
-        return $this->transporter->requestObject($payload);
+        return $this->transporter->requestObject($payload)->data();
     }
 
-    public function delete(string $businessId): \MusheAbdulHakim\GoHighLevel\ValueObjects\Transporter\Response
+    public function delete(string $businessId): array|string
     {
         $payload = Payload::delete('businesses/', $businessId);
 
-        return $this->transporter->requestObject($payload);
+        return $this->transporter->requestObject($payload)->data();
     }
 
-    public function get(string $businessId): \MusheAbdulHakim\GoHighLevel\ValueObjects\Transporter\Response
+    public function get(string $businessId): array|string
     {
         $payload = Payload::get("businesses/{$businessId}");
 
-        return $this->transporter->requestObject($payload);
+        return $this->transporter->requestObject($payload)->data();
     }
 
-    public function getByLocation(string $locationId): \MusheAbdulHakim\GoHighLevel\ValueObjects\Transporter\Response
+    public function getByLocation(string $locationId): array|string
     {
         $payload = Payload::get('businesses/', [
             'locationId' => $locationId,
         ]);
 
-        return $this->transporter->requestObject($payload);
+        return $this->transporter->requestObject($payload)->data();
     }
 
-    public function create(string $name, string $locationId, array $params): \MusheAbdulHakim\GoHighLevel\ValueObjects\Transporter\Response
+    public function create(string $name, string $locationId, array $params = []): array|string
     {
         $params['name'] = $name;
         $params['locationId'] = $locationId;
         $payload = Payload::create('businesses/', $params);
 
-        return $this->transporter->requestObject($payload);
+        return $this->transporter->requestObject($payload)->data();
     }
 }
